c5f89ff197b5c7cd7540e903f576aaaa518ae870
王梦凡 <wang1078564969@gamil.com>
2018-11-01 13:41:48 +0800

    12

--- src/main.js
+++ src/main.js
@@ -5,13 +5,8 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import VueQuillEditor from 'vue-quill-editor'
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
 
 Vue.use(ElementUI);
-Vue.use(VueQuillEditor);
 
 Vue.config.productionTip = false
 
